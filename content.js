8ce233b658a694074332b9b64817a4221c4b604a
u-Sir <93647031+u-Sir@users.noreply.github.com>
2024-06-23 10:41:21 +0800

    Add files via upload

--- content.js
+++ content.js
@@ -68,6 +68,8 @@ function handleMouseUp(e) {
     }
 }
 
+
+
 function isUrlDisabled(url, disabledUrls) {
     return disabledUrls.some(disabledUrl => {
         if (disabledUrl.includes('*')) {
@@ -117,3 +119,16 @@ chrome.storage.local.get('lastUrl', function(data) {
         lastUrl = data.lastUrl;
     }
 });
+
+// Add an event listener for focus events on the window
+window.addEventListener('focus', function(event) {
+    chrome.storage.local.get('closeWhenFocusedInitialWindow', function(data) {
+        if (data.closeWhenFocusedInitialWindow) {
+            // Send a message to background.js when the window regains focus
+            chrome.runtime.sendMessage({ action: 'windowRegainedFocus' });
+        }
+    });
+});
+
+
+
