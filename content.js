248b2aa8f31e044244258782d0d86736691b2911
u-Sir <men64064654390@163.com>
2025-10-19 19:26:58 +0800

    refine preview mode logic

--- content.js
+++ content.js
@@ -1051,6 +1051,7 @@ function handleEvent(e) {
             if (linkUrl && /^(mailto|tel|javascript):/.test(linkUrl.trim())) return;
             if (isUrlDisabled(linkUrl, linkDisabledUrls)) return;
             if (previewMode &&
+                e.isTrusted &&
                 linkUrl &&
                 !isDoubleClick &&
                 !linkElement.closest("[hx-on\\:click]") &&
