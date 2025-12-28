34a8e8178845daa75edd9c9d6a2b58095b3abccf
u-Sir <men64064654390@163.com>
2025-12-28 20:08:59 +0800

    observer document to remove dot

--- content.js
+++ content.js
@@ -463,18 +463,16 @@ function bindScrollHandlers() {
 
     const observer = new MutationObserver(() => {
         const link = showPreviewIconOnHover._lastLink;
-
         if (link && !link.isConnected) {
             cleanup();
             showPreviewIconOnHover._lastLink = null;
         }
     });
-    const root = link.getRootNode();
-    observer.observe(
-        root instanceof ShadowRoot ? root : document,
-        { childList: true, subtree: true }
-    );
 
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
 
     bindScrollHandlers._observer = observer;
 
